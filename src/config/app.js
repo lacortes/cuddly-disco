5eaf2168b4174b0f3b4acd8c8696fa5e349ee710
Luis Cortes <cortes72luis@gmail.com>
2022-01-07 18:29:45 -0800

    Only allow karly capstone client to have access to api.

--- src/config/app.js
+++ src/config/app.js
@@ -4,5 +4,6 @@ module.exports = {
     port: process.env.PORT,
     db_uri: process.env.DB_URI,
     jwt_secret_key: process.env.JWT_SECRET_KEY,
-    email_key: process.env.EMAIL_KEY    
+    email_key: process.env.EMAIL_KEY,
+    node_env: process.env.NODE_ENV
 };
